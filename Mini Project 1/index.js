81ea1d845dc908432c179c6671634485f9ae0408
Rikki <rikkisamone@gmail.com>
2024-12-07 04:42:52 -0800

    adding functionality to buttons

---
+++ Mini Project 1/index.js
@@ -0,0 +1,22 @@
+//Home Page
+function login() {
+    window.location.href = '../frontend/login.html';
+};
+
+function createAccount() {
+    window.location.href = '../frontend/createAccount.html';
+};
+
+function toteam() {
+    window.location.href = '../frontend/meetOurCoaches.html';
+};
+
+//Log In Page
+function tohome() {
+    window.location.href = '../frontend/home.html';
+};
+
+//Create Account Page
+function toLogin() {
+    window.location.href = '../frontend/login.html';
+};
